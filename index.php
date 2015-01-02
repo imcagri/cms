5a2f058d23311a4d76735940ea0176ca2d6f7688
Çağrı YILDIRIM <cagriyildirim@live.com>
2015-01-02 23:54:35 +0200

    interface eklendi

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
-<?php 
-echo "hello world";
+<?php
+	interface aPayment{
+		public function setTotalAmount();
+	}
+	
+	class garanti implements aPayment{
+	
+		public $price = null;
+		
+		public function __construct(){
+			echo __CLASS__;
+		}
+		
+		public function setTotalAmount(){
+			$this->price = 3.20;
+		}
+	}
+	
 ?>
-
-
-echo testttt
